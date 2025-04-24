744b282784a6d998dacb1afc7b1c87f78e87f0aa
rfanan <rofianan05@gmail.com>
2025-04-24 12:27:21 +0700

    optimize size button menu mobile version

--- src/app/component/Header/index.jsx
+++ src/app/component/Header/index.jsx
@@ -12,7 +12,10 @@ export default function Index() {
     <>
       <div className="fixed top-0 right-0 m-5 z-50">
         <Magnetic>
-          <Button onClick={() => SetIsActive(!isActive)} className={` ${isActive ? "bg-lime-400" : "bg-PrimaryBlack"} relative h-20 w-20 cursor-pointer flex items-center justify-center`}>
+          <Button
+            onClick={() => SetIsActive(!isActive)}
+            className={` ${isActive ? "bg-lime-400" : "bg-PrimaryBlack"} border border-[#F2F7F2]/10 relative h-16 w-16  md:h-20 md:w-20 cursor-pointer flex items-center justify-center`}
+          >
             <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
           </Button>
         </Magnetic>
