dd6adfc2cd4f8fc78e25d18d4458169f438ac381
rfanan <rofianan05@gmail.com>
2025-04-24 10:57:57 +0700

    fix padding on mobile version

--- src/app/section/project/list/index.jsx
+++ src/app/section/project/list/index.jsx
@@ -11,9 +11,9 @@ export default function index({ index, title, category, setModal }) {
         onMouseLeave={() => {
           setModal({ active: false, index });
         }}
-        className={`${styles.project} cursor-pointer flex flex-wrap justify-between md:p-10 xl:p-14 w-full items-center border-b border-t `}
+        className={`${styles.project} cursor-pointer flex flex-wrap justify-between p-10 md:p-10 xl:p-14 w-full items-center border-b border-t `}
       >
-        <h2 className="text-xl font-semibold md:text-5xl xl:text-7xl">{title}</h2>
+        <h2 className="font-semibold text-2xl md:text-5xl xl:text-7xl">{title}</h2>
         <p>{category}</p>
       </div>
     </>
