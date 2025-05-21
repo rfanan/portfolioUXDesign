3630a6578f78fff2cf4eff72b2a3e7f820594dc0
rfanan <rofianan05@gmail.com>
2025-05-21 09:24:33 +0700

    update animation navbar

--- src/app/component/Header/index.jsx
+++ src/app/component/Header/index.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./style.module.scss";
-import { AnimatePresence, easeOut, motion } from "framer-motion";
+import { AnimatePresence, easeInOut, easeOut, motion } from "framer-motion";
 import Nav from "../Header/Nav/index";
 import Button from "../button";
 import Magnetic from "../animate/magnetic";
@@ -13,21 +13,30 @@ const scaleAnimate = {
 
 const slideUp = {
   initial: { y: 0, opacity: 1 },
-  enter: { y: -10, opacity: 0, transition: { duration: 0.4, ease: [0.66, 0.05, 0.36, 1] } },
-  closed: { y: 0, opacity: 1, transition: { duration: 0.4, ease: [0.66, 0.05, 0.36, 1] } },
+  enter: { y: -40, opacity: 0, transition: { duration: 0.2, cubicBezier: [0, 0, 0.58, 1] } },
+  closed: { y: 0, opacity: 1, transition: { duration: 0.8, cubicBezier: [0, 0, 0.58, 1] } },
 };
 
 export default function Index() {
   const [isActive, SetIsActive] = useState(false);
   const [isButton, setIsButton] = useState(false);
+  const [isNav, setIsNav] = useState(false);
+  const prevScroll = useRef(0);
 
   useEffect(() => {
     const scroll = () => {
-      if (window.scrollY > 30) {
-        setIsButton(true);
+      const y = window.scrollY;
+      setIsButton(y > 400);
+
+      const isScrollDown = y > prevScroll.current;
+
+      if (isScrollDown) {
+        setIsNav(y > 0);
       } else {
-        setIsButton(false);
+        setIsNav(y > 200);
       }
+
+      prevScroll.current = y;
     };
 
     window.addEventListener("scroll", scroll);
@@ -36,8 +45,8 @@ export default function Index() {
 
   return (
     <>
-      <motion.div variants={slideUp} initial="initial" animate={isButton ? "enter" : "closed"} className={`hidden md:block fixed top-0 left-0 w-screen z-50`}>
-        <div className="font-semibold w-full relative flex justify-between  md:py-3 md:px-24 xl:py-10 xl:px-44 cursor-pointer ">
+      <motion.div variants={slideUp} initial="initial" animate={isNav ? "enter" : "closed"} className={`hidden md:block fixed top-0 left-0 w-screen z-50`}>
+        <div className="font-semibold w-full relative flex justify-between  md:py-3 md:px-24 xl:py-8 xl:px-44 cursor-pointer ">
           <div className="hover:text-lime-400">Rofi'anan</div>
           <div className="flex gap-x-10 md:text-sm xl:text-base">
             <Magnetic>
