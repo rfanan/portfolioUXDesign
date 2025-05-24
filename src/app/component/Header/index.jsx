2ca1278a8ae26c1d7a62abe425522757319ca04d
rfanan <rofianan05@gmail.com>
2025-05-24 08:13:18 +0700

    add animate navbar

--- src/app/component/Header/index.jsx
+++ src/app/component/Header/index.jsx
@@ -47,20 +47,31 @@ export default function Index() {
     <>
       <motion.div variants={slideUp} initial="initial" animate={isNav ? "enter" : "closed"} className={`hidden md:block fixed top-0 left-0 w-screen z-50`}>
         <div className="font-semibold w-full relative flex justify-between  md:py-3 md:px-24 xl:py-8 xl:px-44 cursor-pointer ">
-          <div className="hover:text-lime-400">Rofi'anan</div>
+          <div className="relative inline-flex group cursor-pointer">
+            <Magnetic>
+              <div className=" hover:text-lime-400 transition-all duration-75">Rofi'anan</div>
+              <div className="absolute left-1/2 -translate-x-1/2 top-full w-2 h-2 rounded-full bg-black opacity-0 group-hover:opacity-100 transition-all duration-300"></div>
+            </Magnetic>
+          </div>
           <div className="flex gap-x-10 md:text-sm xl:text-base">
-            <Magnetic>
-              <div className="hover:text-lime-400">Home</div>
-            </Magnetic>
-            <Magnetic>
-              <div className="hover:text-lime-400">About</div>
-            </Magnetic>
-            <Magnetic>
-              <div className="hover:text-lime-400">Work</div>
-            </Magnetic>
-            <Magnetic>
-              <div className="hover:text-lime-400">Contact</div>
-            </Magnetic>
+            <div className="relative inline-flex group cursor-pointer">
+              <Magnetic>
+                <div className=" hover:text-lime-400 transition-all duration-75">About</div>
+                <div className="absolute left-1/2 -translate-x-1/2 top-full w-2 h-2 rounded-full bg-black opacity-0 group-hover:opacity-100 transition-all duration-300"></div>
+              </Magnetic>
+            </div>
+            <div className="relative inline-flex group cursor-pointer">
+              <Magnetic>
+                <div className=" hover:text-lime-400 transition-all duration-75">Work</div>
+                <div className="absolute left-1/2 -translate-x-1/2 top-full w-2 h-2 rounded-full bg-black opacity-0 group-hover:opacity-100 transition-all duration-300"></div>
+              </Magnetic>
+            </div>
+            <div className="relative inline-flex group cursor-pointer">
+              <Magnetic>
+                <div className="tra hover:text-lime-400 transition-all duration-75">Contact</div>
+                <div className="absolute left-1/2 -translate-x-1/2 top-full w-2 h-2 rounded-full bg-black opacity-0 group-hover:opacity-100 transition-all duration-300"></div>
+              </Magnetic>
+            </div>
           </div>
         </div>
       </motion.div>
